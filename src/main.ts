8e2a329fbbd78a2d9410806b6ece42b92cb054a8
Hongbusi <q951686662@qq.com>
2022-01-05 18:51:21 +0800

    chore: delete

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import Router from './router';
-import './index.css';
-
-const app = createApp(App);
-
-app.use(Router).mount('#app');
